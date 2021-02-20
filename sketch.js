8bec91837d85caf342bf3be158b87534f75115b3
Anirban Biswas <68367011+anirban0104@users.noreply.github.com>
2021-02-20 12:58:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,266 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+var trex
+var ground, invisibleGround, groundImage;
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+var score=0;
+var gameOver, restart;
+var bgimage 
+var land, landimage
+var wallimage
+var spikeimage,spike
+var lava,lavaimage
+function preload(){
+  bird_running =   loadAnimation("bird1.png", "bird2.png", "bird3.png");
+  bird_collided = loadAnimation("flappy2.png");
+  
+  //groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+  
+  obstacle1 = loadImage("wall.png");
+  obstacle2 = loadImage("wall2.png");
+  obstacle3 = loadImage("wall3.png");
+  obstacle4 = loadImage("wall4.png");
+  obstacle5 = loadImage("wall5.png");
+  obstacle6 = loadImage("wall.png");
+  
+  gameOverImg = loadImage("gameOver.png");
+  restartImg = loadImage("playAgain.png");
+  
+  
+  dieSound = loadSound("sfx_hit.wav");
+ 
+  flapSound = loadSound("flapping.wav")
+  pointsound = loadSound("sfx_point.wav")
+bgimage = loadImage("bg.png")
+landimage = loadImage("landSprite.png")
+wallimage = loadImage("wall.png")
+spikeimage = loadImage("spike2.png")
+lavaimage = loadImage("lava.png")
+}
+
+function setup() {
+  createCanvas(600, 200);
+  
+  trex = createSprite(50,100,20,50);
+  trex.addAnimation("running", bird_running);
+  trex.addAnimation("collided", bird_collided);
+  trex.scale = 0.3;
+  5
+  spike = createSprite(300,-5,600,33)
+  spike.addImage(spikeimage)
+
+  lava = createSprite(300,290,600,33)
+  lava.addImage(lavaimage)
+  lava.depth=1
+  /*ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -(6 + 3*score/100);*/
+  
+  gameOver = createSprite(300,100);
+  gameOver.addImage(gameOverImg);
+  
+  restart = createSprite(300,140);
+  restart.addImage(restartImg);
+  
+  gameOver.scale = 0.5;
+  restart.scale = 0.5;
+
+  gameOver.visible = false;
+  restart.visible = false;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  cloudsGroup = new Group();
+  obstaclesGroup = new Group();
+  
+  score = 0;
+
+land = createSprite(300,200,10,10)
+land.depth =2
+land.addImage(landimage)
+  //land.velocityX=-3
+
+}
+
+function draw() {
+  background(bgimage);
+
+  text("Score: "+ score, 500,50);
+  textSize("25")
+  fill("red")
+  text("© Anirban Biswas 2021", 470,20)
+  land.velocityX=-3
+  //bgimage.velocityX=-3
+
+if(land.x<300){
+land.x=550
+}
+
+
+
+  if (gameState===PLAY){
+    score = score + Math.round(getFrameRate()/60);
+    
+  
+    if(keyDown("space")) {
+      flapSound.play();
+      trex.velocityY = -10;
+    }
+
+    trex.velocityY = trex.velocityY + 0.8
+  
+    /*if (ground.x < 0){
+      ground.x = ground.width/2;
+    }*/
+  
+    trex.collide(invisibleGround);
+    spawnClouds();
+    spawnObstacles();
+    
+    if (score===50 || score===100 || score === 150 || score===200 || score===250 || score===300 || score===350 || score === 400|| score===450 ){
+      //checkPointSound.play();
+      pointsound.play();
+    }
+
+    
+
+
+  
+    if(obstaclesGroup.isTouching(trex)){
+      dieSound.play();  
+      gameState = END;
+        
+    }
+
+if(spike.isTouching(trex)){
+  dieSound.play();
+  gameState= END
+}
+
+if(land.isTouching(trex)){
+  dieSound.play();
+  gameState=END
+}
+if(obstaclesGroup.x>50){
+  pointsound.play();
+}
+
+
+
+  }
+
+
+
+
+
+  else if (gameState === END) {
+    gameOver.visible = true;
+    restart.visible = true;
+    land.velocityX=0
+    
+    //set velcity of each game object to 0
+    //ground.velocityX = 0;
+    trex.velocityY = 0;
+   /* trex.x=50
+    trex.y = 100*/
+    obstaclesGroup.setVelocityXEach(0);
+    cloudsGroup.setVelocityXEach(0);
+    
+    //change the trex animation
+    trex.changeAnimation("collided",bird_collided);
+    
+    //set lifetime of the game objects so that they are never destroyed
+    obstaclesGroup.setLifetimeEach(-1);
+    cloudsGroup.setLifetimeEach(-1);
+    
+    if(mousePressedOver(restart)) {
+      reset();
+      trex.x=50
+      trex.y=100
+    }
+  }
+  
+  
+  drawSprites();
+}
+
+function spawnClouds() {
+  //write code here to spawn the clouds
+  if (frameCount % 60 === 0) {
+    var cloud = createSprite(600,120,40,10);
+    cloud.y = Math.round(random(80,120));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+     //assign lifetime to the variable
+    cloud.lifetime = 200;
+    
+    //adjust the depth
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
+    
+    //add each cloud to the group
+    cloudsGroup.add(cloud);
+  }
+  
+}
+
+function spawnObstacles() {
+  if(frameCount % 60 === 0) {
+    var obstacle = createSprite(600,165,10,40);
+    obstacle.velocityX = -(6 + 3*score/100);
+    
+    //generate random obstacles
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      case 2: obstacle.addImage(obstacle2);
+              break;
+      case 3: obstacle.addImage(obstacle3);
+              break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;
+    }
+    
+    //assign scale and lifetime to the obstacle           
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+    //add each obstacle to the group
+    obstaclesGroup.add(obstacle);
+   // obstaclesGroup.add(lava)
+  }
+}
+
+
+
+
+
+
+
+function reset(){
+  gameState = PLAY;
+  //ground.velocityX = -(6 + 3*score/100);
+  gameOver.visible = false;
+  restart.visible = false;
+  
+  obstaclesGroup.destroyEach();
+  cloudsGroup.destroyEach();
+  
+  trex.changeAnimation("running",bird_running);
+  
+  score = 0;
+  
+}
